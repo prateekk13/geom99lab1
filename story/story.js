b892a05a351880d1715588a5006636a1ef069420
Prateek kalshan <146376167+prateekk13@users.noreply.github.com>
2024-01-24 01:08:03 -0500

    Update story.js

--- story/story.js
+++ story/story.js
@@ -1,6 +1,6 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 10,
+    zoom: 5,
     center: { lat: 29.3909, lng: 76.9635 },
   });
   // Create an array of alphabetical characters used to label the markers.
