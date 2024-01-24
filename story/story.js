c74d9da6ffa46fdbaeff49dc87283c3ac6fdede1
Prateek kalshan <146376167+prateekk13@users.noreply.github.com>
2024-01-24 18:16:09 -0500

    Update story.js

--- story/story.js
+++ story/story.js
@@ -20,11 +20,11 @@ function calculateAndDisplayRoute(directionsService, directionsRenderer) {
     .route({
       origin: { lat:44.343809, lng: -78.740462 },
       destination: { lat: 44.341307, lng: -78.741153 },
-            waypoints: [ 
+      waypoints: [ 
         { location:new google.maps.LatLng( 44.3492254, -78.7554601) } ,
         { location:new google.maps.LatLng( 44.3593604, -78.741343) } ,
         { location:new google.maps.LatLng( 44.3588230, -78.7344795) }
-],
+    ],
       // Note that Javascript allows us to access the constant
       // using square brackets and a string value as its
       // "property."
