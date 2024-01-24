b19f1cb98e4acbcac718fd5dab518964d8fddab9
Prateek kalshan <146376167+prateekk13@users.noreply.github.com>
2024-01-24 16:43:15 -0500

    Update story.js

--- story/story.js
+++ story/story.js
@@ -2,7 +2,7 @@ function initMap() {
   const directionsRenderer = new google.maps.DirectionsRenderer();
   const directionsService = new google.maps.DirectionsService();
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 14,
+    zoom: 10,
     center: { lat: 44.343809, lng: -78.740462 },
   });
 
@@ -21,7 +21,9 @@ function calculateAndDisplayRoute(directionsService, directionsRenderer) {
       origin: { lat:44.343809, lng: -78.740462 },
       destination: { lat: 44.34130783, lng: -78.741153 },
             waypoints: [ 
-        { location;new google.maps.LatLng( 44.349225413, -78.755460118)   }
+        { location;new google.maps.LatLng( 44.349225413, -78.755460118) }
+        { location;new google.maps.LatLng( 44.359360438, -78.74134368) }
+        { location;new google.maps.LatLng( 44.358823000, -78.734479531) }
 ]
       // Note that Javascript allows us to access the constant
       // using square brackets and a string value as its
